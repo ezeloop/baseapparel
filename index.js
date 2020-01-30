951a8b2a6088aa556a5e4a595d7cc9afd396df29
Ezequiel Lopez <56897904+ezeloop@users.noreply.github.com>
2020-01-30 00:35:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+var email=document.querySelector(".email");
+var emailMsg=document.getElementById('e-mail');
 
 
-if ($('html').width() <= 550)
-{
+var btn=document.querySelector(".button");
 
-      alert("its mobile time");
-    }
+btn.addEventListener('click', formValitation);
+function formValitation(e){
+  if(!email.value)
+  {
+    errorMessage(email, emailMsg);
+  }
+  else{
+    successMessage(email, emailMsg);
+  }
+    e.preventDefault();
+  }
+  function errorMessage(input, message){
+        message.style.color = "red";
+        message.style.font = "italic";
+        input.style.border = " solid hsl(0, 100%, 74%)";
+        input.placeholder = "         Enter Your Email";
+        input.style.background = "url(images/error3.png)  no-repeat left";
+
+
+
+     }
+     function successMessage(input, message){
+         message.style.color = 'white';
+         input.style.border = "solid #f1fcfc";
+         input.style.background = "";
+        }
